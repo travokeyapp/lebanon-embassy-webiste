228763f702988abc15dcd65371cd67b5d6739d2c
travokeyapp <travokeyapp@gmail.com>
2026-03-18 13:41:08 +0500

    updated

--- src/app/[locale]/ambassador/page.tsx
+++ src/app/[locale]/ambassador/page.tsx
@@ -68,7 +68,7 @@ export default async function AmbassadorPage({ params }: { params: Promise<{ loc
           <aside className="card">
             <div className="ambassadorPortraitWrap">
               <Image
-                src="/lebanon_ambassador.jpeg"
+                src="/lebanon_ambasssador_with_flag.jpeg"
                 alt={t.portraitAlt}
                 width={900}
                 height={1200}
